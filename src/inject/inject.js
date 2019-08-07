875e80b5c66e71a2f60d639199af807926cb1525
unknown <ajayrajkollath@gmail.com>
2019-08-07 20:14:54 +0530

    commit

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,6 +6,38 @@ chrome.extension.sendMessage({}, function(response) {
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
+		var intime = setInterval((function(){
+			var em= document.querySelector('#guide-content');
+			if(em)
+			{
+				clearInterval(intime);
+				em.parentNode.removeChild(em);	
+			}
+		
+		im=document.getElementById("masthead-ad");
+		if(im)
+		{
+			clearInterval(intime);
+			im.parentNode.removeChild(im);
+		}
+
+		lm= document.querySelector('ytd-browse');
+		if(lm)
+		{
+			clearInterval(intime);
+			//lm.style.display = 'none' ;
+		   lm.parentNode.removeChild(lm)
+		}
+
+		sd = document.getElementById('secondary');
+		if(sd)
+		{
+			clearInterval(intime);
+			sd.parentNode.removeChild(sd);
+		}
+
+		}), 500);
+
 		// ----------------------------------------------------------
 
 	}
